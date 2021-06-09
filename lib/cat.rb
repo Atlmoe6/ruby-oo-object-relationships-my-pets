163e090c34161162db76e521568e799e1bfc87a5
Doe Mohamud <doe.mahamud1@gmail.com>
2021-06-09 19:18:37 -0400

    commit tree 16cff471027c19728f4fc3de2f0602c910515662

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+  @@count = 0 
+ 
+  def initialize (name, owner)
+    @name = name 
+    @owner = owner
+   @mood = "nervous"
+   @@all << self
+   @@count +=1 
+  end 
+
+  def self.all
+    @@all
+  end
+
 end
+
+
+
+ #Mood is not an argument, but it needs to be returned.
+  #Anything that can be changed --> use attr_accessor
+  #Initialized instance variables need getter/setter methods
